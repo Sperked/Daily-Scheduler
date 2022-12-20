464bf07cfb04028e319e16e0a79483d5a964a930
Sergio Perkins <116986004+Sperked@users.noreply.github.com>
2022-12-19 19:52:10 -0700

    Refined code

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,51 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+$(".saveBtn").on("click", save);
+function save() {
+  var value = $(this)
+    .siblings(".form")
+    .val();
+  var time = $(this)
+    .siblings(".time")
+    .text();
+  localStorage.setItem(time, value);
+};
+
+// Change background color for each box base of local time
+$(".row").each(function () {
+
+  if (parseInt($(this).attr("id")) < moment().hours()) {
+    $(this).addClass("red");
+  }
+  else if (parseInt($(this).attr("id")) === moment().hours()) {
+    $(this).addClass("gray");
+  }
+  else {
+    $(this).addClass("green");
+  }
+});
+//   adding text to the app from local storage
+$("#userInput-00").val(localStorage.getItem("12 AM"));
+$("#userInput-01").val(localStorage.getItem("1 AM"));
+$("#userInput-02").val(localStorage.getItem("2 AM"));
+$("#userInput-03").val(localStorage.getItem("3 AM"));
+$("#userInput-04").val(localStorage.getItem("4 AM"));
+$("#userInput-05").val(localStorage.getItem("5 AM"));
+$("#userInput-06").val(localStorage.getItem("6 AM"));
+$("#userInput-07").val(localStorage.getItem("7 AM"));
+$("#userInput-08").val(localStorage.getItem("8 AM"));
+$("#userInput-09").val(localStorage.getItem("9 AM"));
+$("#userInput-10").val(localStorage.getItem("10 AM"));
+$("#userInput-11").val(localStorage.getItem("11 AM"));
+$("#userInput-12").val(localStorage.getItem("12 PM"));
+$("#userInput-13").val(localStorage.getItem("1 PM"));
+$("#userInput-14").val(localStorage.getItem("2 PM"));
+$("#userInput-15").val(localStorage.getItem("3 PM"));
+$("#userInput-16").val(localStorage.getItem("4 PM"));
+$("#userInput-17").val(localStorage.getItem("5 PM"));
+$("#userInput-18").val(localStorage.getItem("6 PM"));
+$("#userInput-19").val(localStorage.getItem("7 PM"));
+$("#userInput-20").val(localStorage.getItem("8 PM"));
+$("#userInput-21").val(localStorage.getItem("9 PM"));
+$("#userInput-22").val(localStorage.getItem("10 PM"));
+$("#userInput-23").val(localStorage.getItem("11 PM"));
+
+$("#local-time").html(moment().format('llll'));
